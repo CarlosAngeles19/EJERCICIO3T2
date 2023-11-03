017fb856dc431fbf4f9b2eff1a4d748dd2bf2d77
PC GAMER <PC GAMER@CHARLES>
2023-11-03 17:15:02 -0500

    Agregar archivos de proyecto.

---
+++ EJERCICIO3T2/Biblioteca/Aritmetica.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIO3T2.Biblioteca
+{
+    internal class Aritmetica
+    {
+
+        public static int sumar(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+
+
+        public static int restar(int numero1, int numero2)
+        {
+            return numero1 - numero2;
+        }
+
+        public static int multiplicar(int numero1, int numero2)
+        {
+            return numero1 * numero2;
+        }
+
+        public static int dividir(int numero1, int numero2)
+        {
+            return numero1 / numero2;
+        }
+    }
+}
---
+++ EJERCICIO3T2/Program.cs
@@ -0,0 +1,38 @@
+﻿using EJERCICIO3T2.Biblioteca;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIO3T2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = Pantallas.menuPrincipal();
+            do
+            {
+                switch (opcion)
+                {
+                    case 0:
+                        opcion = Pantallas.menuPrincipal();
+                        break;
+                    case 1:
+                        opcion = Pantallas.sumar();
+                        break;
+                    case 2:
+                        opcion = Pantallas.restar();
+                        break;
+                    case 3:
+                        opcion = Pantallas.multiplicar();
+                        break;
+                    case 4:
+                        opcion = Pantallas.dividir();
+                        break;
+                }
+            } while (opcion != 5);
+        }
+    }
+}
